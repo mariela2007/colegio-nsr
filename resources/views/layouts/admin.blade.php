bc56e9d5cf220802daa84901df9157818525598a
Elvia <elviahuanca83@gmail.com>
2026-05-20 10:51:05 -0500

    agregar favicon del colegio

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="/imagen/logo.png">
+
     <title>@yield('titulo', 'Panel administrativo') | Colegio NSR</title>
     <!-- Font Awesome para iconos profesionales -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
-    <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;600;800&display=swap" rel="stylesheet">
+<link rel="icon" type="image/x-icon" href="/imagen/logo.png">
+   <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;600;800&display=swap" rel="stylesheet">
     <title>Colegio NSR | Excelencia Educativa</title>
     <style>
         :root {
